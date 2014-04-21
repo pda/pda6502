af623e6ed811f40b35b400a91046f115ccec89fe
Paul Annesley <paul@annesley.cc>
2014-04-20 21:15:59 -0700

    ssd1306 buffer in RAM, splash loaded from ROM.

--- src/kernal.s
+++ src/kernal.s
@@ -4,18 +4,38 @@
 .import Ssd1306Init
 .import Ssd1306WriteScreen
 .import SplashData
+.import CopyPages
 
 .segment "kernal"
+
+ssd1306_buffer = $7000 ; page-aligned 512 byte buffer
 
 Main:
 ;--------
 
-  JSR Ssd1306Init
+; Copy SplashData (ROM) to ssd1306_buffer (RAM).
 
-  ; Store pointer to data at $10
+  ; source pointer
   LDA #.LOBYTE(SplashData)
   STA $10
   LDA #.HIBYTE(SplashData)
+  STA $11
+
+  ; destination pointer
+  LDA #.LOBYTE(ssd1306_buffer)
+  STA $12
+  LDA #.HIBYTE(ssd1306_buffer)
+  STA $13
+
+  LDX #2 ; move two pages of data
+  JSR CopyPages
+
+  JSR Ssd1306Init
+
+  ; Store pointer to data at $10
+  LDA #.LOBYTE(ssd1306_buffer)
+  STA $10
+  LDA #.HIBYTE(ssd1306_buffer)
   STA $11
 
   JSR Ssd1306WriteScreen

abe9427968cc0f77c5b7310cd7b125c11fde56d4
Paul Annesley <paul@annesley.cc>
2014-04-24 22:46:02 -0600

    FontSelectChar overhauled, fully 16-bit.

--- src/font.s
+++ src/font.s
@@ -8,31 +8,45 @@
 ; X: font_ptr zero-page address (currently must be < 32)
 ; Y: char index
 FontSelectChar:
+  TYA
+  PHA      ; save
   LDA $10
-  PHA      ; save $10 on stack
-  TYA
-  PHA      ; save Y on stack
-  TYA
-  ASL
-  ASL
-  ASL
-  STA $10  ; $10 = Y * 8
-  CLC
+  PHA      ; save
+  LDA $11
+  PHA      ; save
+
+  ; initialize base pointer
   LDA #.LOBYTE(FontData)
+  STA 0,X
+  LDA #.HIBYTE(FontData)
+  STA 1,X
+
+  ; char index Y * 8 = $10,11 byte index
+  TYA
+  STA $10 ; lo
+  LDA #0
+  STA $11 ; hi
+  CLC
+  ROL $10
+  ROL $11 ; x2
+  ROL $10
+  ROL $11 ; x4
+  ROL $10
+  ROL $11 ; x8
+
+  ; add 16-bit byte-index at $10 to base font pointer at $00,X.
+  CLC
+  LDA 0,X
   ADC $10
   STA 0,X
-  BCS @hasCarry
-  JMP @noCarry
-@hasCarry:
-  LDY #.HIBYTE(FontData)
-  INY
-JMP @carryDone
-@noCarry:
-  LDY #.HIBYTE(FontData)
-@carryDone:
-  STY 1,X
-  PLA      ; restore Y
-  TAY
-  PLA      ; restore $10
-  STA $10
+  LDA 1,X
+  ADC $11
+  STA 1,X
+
+  PLA
+  STA $11  ; restore
+  PLA
+  STA $10  ; restore
+  PLA
+  TAY      ; restore
   RTS

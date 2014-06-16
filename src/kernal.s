73db55f0ee70d04ee7f0ea985eb8f863f9f90991
Paul Annesley <paul@annesley.cc>
2014-06-15 18:52:37 -0700

    kernal.s: roughly extracted displayText.

--- src/kernal.s
+++ src/kernal.s
@@ -40,14 +40,14 @@ Main:
 
   JSR splashScreen
 
-  ;----------------------------------------
-
   LDX #1
-  JSR SleepXSeconds
+  JSR SleepXSeconds ; sleep(1)
 
+  ; Initialize SD card
   JSR SdCardInit
   JSR SdCardReset
 
+  ; Display data from first block of SD card.
   ; push 32-bit address onto user-stack.
   LDA #$00 ; LSB
   JSR StackPush
@@ -58,29 +58,8 @@ Main:
   LDA #$00 ; MSB
   JSR StackPush
   JSR SdCardRead ; 512 byte block from address on user-stack into $6000.
+  JSR displayText
 
-  ; Initialize pointer to an 8x8 screen segment.
-  LDA #.LOBYTE(ssd1306_buffer)
-  STA ssd1306_ptr
-  LDA #.HIBYTE(ssd1306_buffer)
-  STA ssd1306_ptr_hi
-
-  LDA #0
-  STA $10 ; loop counter = 0
-@writeLoop2:
-  LDX $10
-  LDY $6000,X              ; Y = ASCII-ish byte.
-  JSR writeAsciiToSsdBuffer
-  INC $10
-  LDA $10
-  CMP #message_length
-  BNE @writeLoop2
-
-  LDX #.LOBYTE(ssd1306_buffer)
-  LDY #.HIBYTE(ssd1306_buffer)
-  JSR Ssd1306WriteScreen
-
-  ;----------------------------------------
   JMP Halt
 
 
@@ -100,14 +79,14 @@ JMP Halt
 
   LDA #0
   STA $10 ; loop counter = 0
-@writeLoop:
+loop:
   LDX $10
   LDY Message,X              ; Y = ASCII-ish byte.
   JSR writeAsciiToSsdBuffer
   INC $10
   LDA $10
   CMP #message_length
-  BNE @writeLoop
+  BNE loop
 
   LDX #.LOBYTE(ssd1306_buffer)
   LDY #.HIBYTE(ssd1306_buffer)
@@ -116,6 +95,30 @@ JMP Halt
   RTS
 .ENDPROC
 
+; Display the text at $6000
+.PROC displayText
+  ; Initialize pointer to an 8x8 screen segment.
+  LDA #.LOBYTE(ssd1306_buffer)
+  STA ssd1306_ptr
+  LDA #.HIBYTE(ssd1306_buffer)
+  STA ssd1306_ptr_hi
+
+  LDA #0
+  STA $10 ; loop counter = 0
+loop:
+  LDX $10
+  LDY $6000,X              ; Y = ASCII-ish byte.
+  JSR writeAsciiToSsdBuffer
+  INC $10
+  LDA $10
+  CMP #message_length
+  BNE loop
+
+  LDX #.LOBYTE(ssd1306_buffer)
+  LDY #.HIBYTE(ssd1306_buffer)
+  JSR Ssd1306WriteScreen
+  RTS
+.ENDPROC
 
 ; Y: ASCII-ish char to write.
 ; X: destroyed
